f2e53b2bdee1720fdc186605df879ac8595aba74
Tadas Cislikauskas <tacista@gmail.com>
2022-03-07 17:38:46 +0200

    Fixed. Working now. xmlns compatibility fixed

--- src/main/java/com/clock/digitalclock/Main.java
+++ src/main/java/com/clock/digitalclock/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Time is up");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
